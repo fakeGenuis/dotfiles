d3f4e31c4fbda115466f6a2b851b0fff389de28f
XXX <32349731+fakeGenuis@users.noreply.github.com>
2021-04-22 02:33:09 +0800

    [add] oh my fish config

---
+++ .config/omf/init.fish
@@ -0,0 +1,20 @@
+# color themes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme nord
+set -g theme_powerline_fonts yes
+set -g theme_title_display_process yes
+
+# emacs vterm shell-side configuration
+if test "$INSIDE_EMACS" = 'vterm' -a -f "$EMACS_VTERM_PATH/etc/emacs-vterm-fish.fish"
+   source "$EMACS_VTERM_PATH/etc/emacs-vterm.fish"
+end
+
+# https://www.emacswiki.org/emacs/TrampMode
+# https://stackoverflow.com/a/37421917
+# fish with tramp
+if test $TERM = "dumb"
+    function fish_prompt
+        echo "\$ "
+    end
+end
