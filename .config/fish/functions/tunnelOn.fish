46134dbb2f8bdfcceccd4f8c4ecc8e3abe717de9
XXX <32349731+fakeGenuis@users.noreply.github.com>
2022-03-20 17:05:34 +0800

    [change] tunnelon fish func

--- .config/fish/functions/tunnelOn.fish
+++ .config/fish/functions/tunnelOn.fish
@@ -1,13 +1,36 @@
 #!/usr/bin/env fish
 
 # Defined in - @ line 1
-function tunnelOn --description "change status of ssh to server"
+function tunnelOn --description "toggle status of ssh to server"
     switch "$argv[1]"
-        case grasp
-            ssh -fCN $argv[2] -L 3664:localhost:3664
-        case vnc
-            ssh -fCN $argv[2] -L 5014:localhost:50114
-        case off
-            kill (ps aux | grep ssh | awk '/PRD/ {print $2}')
+        case -grasp
+            set -l pid (pgrep -a ssh | grep "$argv[2]" | grep 3664 | cut -d' ' -f1)
+            if test -n "$pid"
+                set_color red
+                echo "An ssh tunnel of grasp is already on!"
+                set_color normal
+                y_or_n "close it\?" && kill "$pid"
+            else
+                ssh -fCN "$argv[2]" -L 3664:localhost:3664
+            end
+        case -vnc
+            set -l pid (pgrep -a ssh | grep "$argv[2]" | grep 5014 | cut -d' ' -f1)
+            if test -n "$pid"
+                set_color red
+                echo "An ssh tunnel of vnc is already on!"
+                set_color normal
+                y_or_n "close it\?" && kill "$pid"
+            else
+                ssh -fCN "$argv[2]" -L 5014:localhost:50114
+            end
+        case -h
+            echo "Usage: tunnelOn [Options] sshName
+toggle on ssh port forward for services
+
+[Options]
+    -h              print help message
+    -grasp          grasp service, use local port 3664
+    -vnc            vnc service, use local port 5014
+    "
     end
 end
