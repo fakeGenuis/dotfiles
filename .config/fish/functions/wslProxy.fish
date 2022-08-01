3ef304cfc03fd4fe93cb9ca652b0c20cb00a440f
XXX <32349731+fakeGenuis@users.noreply.github.com>
2022-08-01 20:47:58 +0800

    [update] wsl ...

--- .config/fish/functions/wslProxy.fish
+++ .config/fish/functions/wslProxy.fish
@@ -4,7 +4,7 @@ function wslProxy --description "Set wsl use windows host proxy"
         case status
             ps aux | grep ssh
         case on
-            sudo subsystemctl start && sudo subsystemctl exec /usr/bin/sshd && git-bash.exe -c "ssh -R 8883:localhost:8889 -fCN wsl"
+            sudo subsystemctl start && sudo subsystemctl exec /usr/bin/sshd && git-bash.exe -c "ssh -R 8889:localhost:8889 -fCN wsl"
         case off
             git-bash.exe -c "kill `ps aux | awk '/ssh/ {print $1}'`"
     end
