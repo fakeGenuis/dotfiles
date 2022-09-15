38f19289b5eff1786b3206e229c63c15ab19144a
XXX <32349731+fakeGenuis@users.noreply.github.com>
2022-09-15 16:54:58 +0800

    [fix] windows bug tunnel if same port

--- .config/fish/functions/wslProxy.fish
+++ .config/fish/functions/wslProxy.fish
@@ -2,9 +2,9 @@
 function wslProxy --description "Set wsl use windows host proxy"
     switch $argv[1]
         case status
-            ps aux | grep ssh
+            pgrep -af ssh
         case on
-            sudo subsystemctl start && sudo subsystemctl exec /usr/bin/sshd && git-bash.exe -c "ssh -R 8889:localhost:8889 -fCN wsl"
+            pgrep sshd 2&>/dev/null || sudo /usr/bin/sshd && git-bash.exe -c "ssh -R 8889:localhost:8883 -fCN wsl"
         case off
             git-bash.exe -c "kill `ps aux | awk '/ssh/ {print $1}'`"
     end
