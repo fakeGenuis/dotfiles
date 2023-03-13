3f4d251254e9655cc8cdcb36d119e114face31b9
PRD <32349731+fakeGenuis@users.noreply.github.com>
2023-03-13 08:49:53 +0800

    [fix] fish in remote shell
    if no DISPLAY, hidpi.fish init with error

--- .config/fish/conf.d/hidpi.fish
+++ .config/fish/conf.d/hidpi.fish
@@ -2,7 +2,7 @@
 # https://wiki.archlinux.org/title/HiDPI
 
 if status is-login
-    set -l reso (xdpyinfo | grep dimension | awk '{print $2}' | cut -d'x' -f1)
+    set -l reso (DISPLAY=:0 xdpyinfo | grep dimension | awk '{print $2}' | cut -d'x' -f1)
     if test $reso -gt 2560
         set -gx GDK_SCALE 2
         set -gx GDK_DPI_SCALE 0.5
