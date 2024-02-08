087da83220410c0f0faf43437eac27e0878e3408
Diana <32349731+fakeGenuis@users.noreply.github.com>
2024-02-08 15:52:18 +0800

    [add] proxychains config

--- .config/fish/conf.d/gwsl.fish
+++ .config/fish/conf.d/gwsl.fish
@@ -9,7 +9,9 @@ if status is-login; and string match -qri wsl (uname -a)
     set -gx QT_SCALE_FACTOR 2 #GWSL
     # proxy in wsl (set listen on 0.0.0.0 on windows proxy software)
     set -gx ALL_PROXY (cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'):8889
-    # ssh proxy with http tunnel
+    # ssh proxy with http tunnel (often github.com)
     sed -i "/ProxyCommand/c\  ProxyCommand nc -X connect -x $ALL_PROXY %h %p" ~/.ssh/config
+    # proxy config of proxychains (often with `mbsync`)
+    sed -i "/http/c\http $(string replace : " " $ALL_PROXY)" .cache/proxychains.conf
     # emacsclient -e 1 &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
 end
