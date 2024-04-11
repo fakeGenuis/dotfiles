1561249a0a9cfa99e058fb5dd67583c10bfc7180
PRD <32349731+fakeGenuis@users.noreply.github.com>
2024-04-11 09:43:06 +0800

    [update] Xft.dpi template
    now Xft.dpi in .Xresources is set from $XDPI env

--- .config/fish/conf.d/hidpi.fish
+++ .config/fish/conf.d/hidpi.fish
@@ -2,8 +2,10 @@
 # https://wiki.archlinux.org/title/HiDPI
 
 if status is-login; and test -n "$DISPLAY"
-    set -l reso (xdpyinfo | grep dimension | awk '{print $2}' | cut -d'x' -f1)
-    if test "$reso" -gt 2560
+    set XDPI (xdpyinfo | grep resolution | awk '{print $2}' | cut -d'x' -f1)
+    # fractional scaled dpi (1.25, 1.5, ., 2) x 96
+    set -gx XDPI (echo "scale=0; 24*($XDPI/24 + 1)/1" | bc)
+    if test "$XDPI" -gt 144
         set -gx GDK_SCALE 2
         set -gx GDK_DPI_SCALE 0.5
         set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
