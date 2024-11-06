3266ac0cf4a428cecd69271d31ff060ecde2ed2e
JiaRan <32349731+fakeGenuis@users.noreply.github.com>
2024-11-07 01:13:47 +0800

    update hidpi conf of QT

--- .config/fish/conf.d/hidpi.fish
+++ .config/fish/conf.d/hidpi.fish
@@ -4,10 +4,13 @@
 if status is-login; and test -n "$DISPLAY"
     set XDPI (xdpyinfo | grep resolution | awk '{print $2}' | cut -d'x' -f1)
     # fractional scaled dpi (1.25, 1.5, ., 2) x 96
-    set -gx XDPI (echo "scale=0; 24*($XDPI/24 + 1)/1" | bc)
+    set -gx XDPI (echo "scale=0; ($XDPI + 12)/24*24" | bc -l)
+    set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
+    set -gx QT_ENABLE_HIGHDPI_SCALING 1
+    # GDK should aware `Xft.dpi'
     if test "$XDPI" -gt 144
+        # GDK 3 not support fractional scaling
         set -gx GDK_SCALE 2
         set -gx GDK_DPI_SCALE 0.5
-        set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
     end
 end
