3d2232b22e2d1423a40356b7e4bd4ff3810327f3
Diana <32349731+fakeGenuis@users.noreply.github.com>
2025-01-26 02:55:08 +0800

    update proxy in wsl

--- .config/fish/conf.d/gwsl.fish
+++ .config/fish/conf.d/gwsl.fish
@@ -7,11 +7,6 @@ if status is-login; and string match -qri wsl (uname -a)
     set -gx LIBGL_ALWAYS_INDIRECT 1 #GWSL
     set -gx GDK_SCALE 2 #GWSL
     set -gx QT_SCALE_FACTOR 2 #GWSL
-    # proxy in wsl (set listen on 0.0.0.0 on windows proxy software)
-    set -gx ALL_PROXY (cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'):8889
-    # ssh proxy with http tunnel (often github.com)
-    sed -i "/ProxyCommand/c\  ProxyCommand nc -X connect -x $ALL_PROXY %h %p" ~/.ssh/config
-    # proxy config of proxychains (often with `mbsync`)
-    sed -i "/http/c\http $(string replace : " " $ALL_PROXY)" .cache/proxychains.conf
-    # emacsclient -e 1 &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
+    # proxy in wsl (set network mode to mirrored solves everything!)
+    # now ALL_PROXY can be a fixed environment variable
 end
