5c30dc6ad9ae4054b62b9fa3ae20ced8eb68e66c
Diana <32349731+fakeGenuis@users.noreply.github.com>
2025-02-02 11:13:11 +0800

    rm GDK_SCALE in wsl

--- .config/fish/conf.d/gwsl.fish
+++ .config/fish/conf.d/gwsl.fish
@@ -5,7 +5,7 @@ if status is-login; and string match -qri wsl (uname -a)
     # set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 #GWSL
     # set -gx PULSE_SERVER tcp:(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') #GWSL
     set -gx LIBGL_ALWAYS_INDIRECT 1 #GWSL
-    set -gx GDK_SCALE 2 #GWSL
+    # set -gx GDK_SCALE 2 #GWSL
     set -gx QT_SCALE_FACTOR 2 #GWSL
     # proxy in wsl (set network mode to mirrored solves everything!)
     # now ALL_PROXY can be a fixed environment variable
