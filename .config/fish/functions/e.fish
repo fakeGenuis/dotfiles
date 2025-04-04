10c7cd50812a0abc765206eea8c2036581c4c98f
JiaRan <32349731+fakeGenuis@users.noreply.github.com>
2025-04-04 18:19:44 +0800

    rm unused fish functions

--- .config/fish/functions/e.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function e --wraps=emacsclient --description 'alias e=emacsclient'
-    emacsclient -c -a emacs $argv
-end
--- .config/fish/functions/m.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function m --wraps=mv --description 'alias m=mv'
-    mv $argv
-end
